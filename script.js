d50f947cf45a5195a1a27e558006bccaba1dbe90
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-11-25 00:07:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const board = document.querySelector("#board");
+const squares=800;
+for(let i=0;i<squares;i++){
+	const square=document.createElement("div");
+	square.classList.add("square");
+
+
+	square.addEventListener("mouseover",()=>{
+		const color="red";
+		square.style.background=color;
+
+		setTimeout(()=>{
+			square.style.background="black";
+		},1000)
+	})
+	board.appendChild(square)
+}
